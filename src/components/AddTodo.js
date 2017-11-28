6365e20d98f2e1d4568636d9cc58bde74ed2e0b5
Vinícius Laghi <vinicius.laghi@gmail.com>
2017-11-28 19:09:54 -0200

    new buttons layout
    check na esquerda, delete na direita, botão para check all, alinhamento dos botões

--- src/components/AddTodo.js
+++ src/components/AddTodo.js
@@ -1,37 +1,49 @@
 import React from "react";
 import "./addTodo.css"
+import FaAngleDown from 'react-icons/lib/fa/angle-down';
+
 
 class AddTodo extends React.Component {
   handleKeyPress(target) {
     if (target.charCode === 13) {
-     
-      if (this.input.value.trim() !== ""){
-      this.props.addItem(this.input.value);
+
+      if (this.input.value.trim() !== "") {
+        this.props.addItem(this.input.value);
       }
       this.input.value = "";
-      
+
     }
   }
   render() {
     const addItem = this.props.addItem;
     return (
-      <div>
-        
-        <input className="Input" type="text"
+      <div style={{
+        textAlign: "left",
+        maxWidth: 500,
+        width: "100%",
+        margin: "auto",
+        display: 'flex'
+      }}
+      >
+
+        <button className="CheckAll" >
+          <FaAngleDown />
+        </button>
+        <input className="Input" type="text" placeholder="What needs to be done?"
           ref={text => {
             this.input = text;
           }}
           onKeyPress={target => this.handleKeyPress(target)}
         />
-        <button className="Button"
+        {/* <button className="Button"
           onClick={() => {
             addItem(this.input.value);
             this.input.value = "";
           }}
         >
           <b>Add</b>
-        </button>
-       
+        </button> */}
+
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -5,39 +5,39 @@ import FaCheckSquareO from 'react-icons/lib/fa/check-square-o';
 import "./todoItem.css";
 
 class TodoItem extends React.Component {
- 
+
   render() {
     const { deleteItem, action, id, done, toggleDone } = this.props;
     const check = (done) => {
-        if (done === false) {
-            return <button className="Uncheck"><FaSquareO /></button>;
-           
-            
-                    }
-                    else { 
-                        return <button className="Check"><FaCheckSquareO /></button>;
-                        
-                    }
-                }
-                // const color = (done) => {
-                //   if (done === false) {
-                //     return ""
-                //     console.log(color);
-                //   }
-                //   else{
-                //     return ""
-                //     console.log(color);
-                //   }
-                // }
+      if (done === false) {
+        return <button className="Uncheck"><FaSquareO /></button>;
+
+
+      }
+      else {
+        return <button className="Check"><FaCheckSquareO /></button>;
+
+      }
+    }
+    // const color = (done) => {
+    //   if (done === false) {
+    //     return ""
+    //     console.log(color);
+    //   }
+    //   else{
+    //     return ""
+    //     console.log(color);
+    //   }
+    // }
     return (
-      <div style={{ display: "flex", marginBottom: "5px"}}>
-        
+      <div className="DivItem" >
+
+        <div className="DivCheck" onClick={x => toggleDone(id)}>{check(done)}</div>
         <span className="Action" onClick={x => toggleDone(id)}> {action} </span>
-      <div className="DivItem" onClick={x => toggleDone(id)}>{check(done)}</div>
-      <button className="Delete" onClick={x => deleteItem(id)}>
+        <button className="Delete" onClick={x => deleteItem(id)}>
           <FaTrash />
         </button>
-        
+
       </div>
     );
   }
